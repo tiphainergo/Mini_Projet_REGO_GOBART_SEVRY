8724d0c0ee218e34e341824a351afa585535756a
regotiphaine <regotiphaine@10.3.60.126>
2023-11-13 17:08:17 +0100

    Création du dépot

---
+++ src/cpo_mini_projet_2_rego_gobart_sevry/CPO_Mini_Projet_2_REGO_GOBART_SEVRY.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cpo_mini_projet_2_rego_gobart_sevry;
+
+/**
+ *
+ * @author regotiphaine
+ */
+public class CPO_Mini_Projet_2_REGO_GOBART_SEVRY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
